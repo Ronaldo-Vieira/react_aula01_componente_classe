37b643baac46d1d2981b4cdb204a94d675e1afd5
Ronaldo-Vieira-Lira <ronaldo.vieiralira@gmail.com>
2021-11-01 20:06:24 -0300

    atividade1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div id="Classe">
+          <h1>Olá mundo React</h1>
+          <h3>Esse é meu componente de classe :] </h3>
+        </div>
+    );
+}
+};
+
+export default App;
